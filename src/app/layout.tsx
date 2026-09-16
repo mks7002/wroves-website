2bb940f9acfae89a5b753efeb604beaf175982c3
mks7002 <mayankkumarsingh7002@gmail.com>
2026-09-16 14:52:01 +0530

    Fix script tag placement in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
-<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1901736594863701"
+<head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1901736594863701"
      crossOrigin="anonymous"></script>
+     </head>
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
